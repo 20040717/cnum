f8948091c73197e0514f88b40d8571a9448b45c9
Héryck github <142687649+20040717@users.noreply.github.com>
2024-07-29 17:20:04 +0000

    corrigido

--- aula 1/main.jl
+++ aula 1/main.jl
@@ -16,10 +16,24 @@
 # println(z)
 
 
-function z(a,b,c)
-delta=b^2 - 4*a*c
-x1=-b+sqrt(delta)/2*a
-x2=-b-sqrt(delta)/2*a
-end
+  function z()
+    println("Digite o valor de a:")
+    a = parse(Float64, readline())
 
-exit() 
+    println("Digite o valor de b:")
+    b = parse(Float64, readline())
+
+    println("Digite o valor de c:")
+    c = parse(Float64, readline())
+
+    delta = b^2 - 4*a*c
+    x1 = (-b + sqrt(delta)) / (2*a)
+    x2 = (-b - sqrt(delta)) / (2*a)
+
+    println("As raízes da equação são: x1 = $x1 e x2 = $x2")
+    return x1, x2
+  end
+
+  x1, x2 = z()
+  println("x1 = $x1, x2 = $x2")
+
